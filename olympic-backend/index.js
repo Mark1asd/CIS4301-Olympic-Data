e71aa41a43011f5929073f1938b12a911dd479b9
Riley Willis <122632109+rw-w@users.noreply.github.com>
2024-11-20 19:53:09 -0500

    Frontend/Backend initial connections.

---
+++ olympic-backend/index.js
@@ -0,0 +1,57 @@
+const oracledb = require('oracledb');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+const PORT = 3001;
+
+// Alter these as necessary, these were just for my own local instance.
+const dbConfig = {
+    user: 'C##backend_access',
+    password: 'backend_pass',
+    connectString: '127.0.0.1:1521/FREE'
+};
+
+// Test function to make sure you can cannot to your local db instance.
+async function testConnection() {
+    let connection;
+    try {
+        connection = await oracledb.getConnection(dbConfig);
+        console.log('Connected to Oracle Database');
+    } catch(err){
+        console.error('Could not connect to Oracle Database', err);
+    } finally {
+        if(connection){
+            await connection.close();
+        }
+    }
+}
+
+// Test application that executes a simple query on DB.
+app.get('/', async (req, res) => {
+    let connection;
+    try{
+        connection = await oracledb.getConnection(dbConfig);
+
+        const results = await connection.execute ('SELECT * FROM SYS.Athletes FETCH FIRST 3 ROWS ONLY');
+        console.log('Query Results:', results.rows);
+        res.json(results.rows);
+    } catch (err) {
+        console.error('Error executing query', err);
+        res.status(500).send('Error executing query');
+    } finally {
+        if(connection){
+            try{
+                await connection.close();
+            } catch(err){
+                console.error('Error closing connection', err);
+            }
+        }
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Backend running on localhost:${PORT}`);
+})
+
--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3001')
+    .then((response) => {
+      if(!response.ok) {
+        throw new Error('Failed to fetch data from backend');
+      }
+      return response.json();
+    })
+    .then((data) => {
+      setData(data);
+    })
+    .catch((error) => {
+      setError(error.message);
+    });
+  }, []);
+
   return (
     <div>
-      Hello Everyone
+      <h1>Athletes Data Example - 3 Rows</h1>
+
+      {error && <p>Error: {error}</p>}
+
+      {!error && data.length === 0 && <p>Loading data...</p>}
+
+      {!error && data.length > 0 && (
+        <ul>
+          {data.map((row, index) => (
+            <li key={index}>{JSON.stringify(row)}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
